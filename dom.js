dec8646f9cb9c1367e1aaee3c5dc0e4330b5651c
Sristi Shreya <shreyasristi@gmail.com>
2023-09-02 18:02:30 +0530

    changes

--- dom.js
+++ dom.js
@@ -2,16 +2,21 @@
 // console.log(location)
 // console.log(document.URL)
 // console.log(document.location)
-var headTitle = document.getElementById('header-title');
-console.log(headTitle.textContent);
-console.log(headTitle.innerText);
-headTitle.style.border = 'solid 2px black';
+// var headTitle = document.getElementById('header-title');
+// console.log(headTitle.textContent);
+// console.log(headTitle.innerText);
+// headTitle.style.border = 'solid 2px black';
 // headTitle.style.color = 'red';
 // headTitle.textContent = 'Hello';
 // headTitle.innerHTML = 'World';
-var Xtitle=document.getElementsByClassName('title');
+// var Xtitle=document.getElementsByClassName('title');
 // console.log(Xtitle);
 // console.log(Xtitle[0].textContent);
-var addItems = Xtitle[0];
-addItems.innerHTML = '<b>Add Items</b>'
+// var addItems = Xtitle[0];
+// addItems.innerHTML = '<b>Add Items</b>'
 // addItems.style.color = 'red';
+var Xtitle=document.getElementsByClassName('list-group-item');
+Xtitle[2].style.backgroundColor='green';
+for (let i=0;i<Xtitle.length;i++){
+    Xtitle[i].style.fontWeight='bold';
+}
